3198c1613991a2961a7b9f0a4deeb58fb6f0db86
Anicka Burova <anicka.burova@gmail.com>
2021-02-23 08:34:11 +0100

    An infinite rabbit consumer

---
+++ src/consumer.rs
@@ -0,0 +1,137 @@
+//! A stream wrapper for rabbitmq consumer. This stream never fails and will consume until stopped being used.
+use core::pin::Pin;
+use futures::{
+    future::Future,
+    stream::Stream,
+    task::{Context, Poll},
+};
+use lapin::{message::Delivery, Channel, Consumer};
+use anyhow::Result;
+
+use super::comms::*;
+
+/// Returns a future which creates the consumer from the provided channel.
+pub type ConsumerCreator = Creator<(Channel, Consumer)>;
+
+type NextFuture = Pin<
+    Box<
+        dyn Future<
+            Output = (
+                (Channel, Delivery),
+                Consumer,
+                Channel,
+                Creator<(Channel, Consumer)>,
+            ),
+        > + Send,
+    >,
+>;
+
+enum State {
+    /// Intermediate state where this object is in the middle of something or just generally invalid after an error
+    Invalid,
+    /// Waiting to start looking for the next item
+    Idle {
+        /// RabbitMQ consumer
+        consumer: Consumer,
+        /// RabbitMQ channel, this has to be keep here for having the consumer alive, otherwise there will
+        /// be non channel alive for the consumer, and the consumer would be dropped.
+        channel: Channel,
+        /// Creator of the consumer
+        creator: ConsumerCreator,
+    },
+    /// Looking for the next item
+    Next {
+        /// Future to get the next item
+        next: NextFuture,
+    }
+}
+
+/// Consumer wrapper handles errors in the connection. If the rabbitmq is disconnected, instead of
+/// finishing the stream, the wrapper will try to reconnect and recreate the connection and continue
+/// consuming like nothing happened. But if the connection was never established at least once, the stream
+/// end right away!
+pub struct ConsumerWrapper {
+    state: State,
+}
+
+impl ConsumerWrapper {
+    /// Create a new consumer by providing a creator function. This function might be called many times,
+    /// as often as we loose connection to the rabbitmq.
+    pub async fn new(creator: ConsumerCreator) -> Result<Self> {
+        let (creator, (channel, consumer)) = Self::connect(creator).await?;
+        log::debug!("Consumer wrapper created");
+        Ok(Self { state: State::Idle { consumer, channel, creator } })
+    }
+
+    /// Connects to the rabbit by passing the creator function
+    async fn connect(
+        creator: ConsumerCreator,
+    ) -> Result<(ConsumerCreator, (Channel, Consumer))> {
+        Comms::create_channel_and_object::<(Channel, Consumer)>(creator).await
+    }
+
+    /// Take the current state and replace it with the invalid state
+    fn take(&mut self) -> State {
+        std::mem::replace(&mut self.state, State::Invalid)
+    }
+
+    /// Gets the next item from the consumer. If the consumer is broken, then a new consumer is automatically created
+    async fn next_item(mut consumer: Consumer, mut channel: Channel, mut creator: ConsumerCreator)
+        -> ((Channel, Delivery), Consumer, Channel, ConsumerCreator) {
+        loop {
+            use futures::stream::StreamExt;
+            log::debug!("Polling consumer");
+            match consumer.next().await {
+                Some(Ok(delivery)) => {
+                    log::debug!("Got delivery");
+                    return (delivery, consumer, channel, creator);
+                }
+                Some(Err(err)) => {
+                    log::error!("Failed to consume a message: {}", err);
+                }
+                None => {
+                    log::error!("Consumer has finished for some reason!");
+                }
+            }
+            log::warn!("Recreating the consumer");
+            let (cr, (chan, cons)) = Self::connect(creator).await.unwrap();
+            creator = cr;
+            consumer = cons;
+            channel = chan;
+        }
+    }
+}
+
+impl Stream for ConsumerWrapper {
+    type Item = (Channel, Delivery);
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        log::debug!("Poll next");
+        let this = Pin::into_inner(self);
+
+        loop {
+            match this.take() {
+                State::Idle { consumer, channel, creator } => {
+                    this.state = State::Next {
+                        next: Box::pin(Self::next_item(consumer, channel, creator)),
+                    };
+                }
+                State::Next { mut next } => {
+                    let action = next.as_mut();
+                    return match Future::poll(action, cx) {
+                        Poll::Pending => {
+                            this.state = State::Next { next };
+                            log::debug!("Pending");
+                            Poll::Pending
+                        }
+                        Poll::Ready((delivery, consumer, channel, creator)) => {
+                            this.state = State::Idle { consumer, channel, creator };
+                            log::debug!("Ready");
+                            Poll::Ready(Some(delivery))
+                        }
+                    }
+                }
+                State::Invalid => unreachable!(),
+            }
+        }
+    }
+}
