61a8ef2c8c82a4b703666c68b2378330fb23c0ec
Anicka Burova <anicka.burova@gmail.com>
2022-09-01 10:45:08 +0200

    Creating any kind of exchange and deleting the old one in case of not compatibility

--- src/exchanges.rs
+++ src/exchanges.rs
@@ -5,26 +5,31 @@ use futures::lock::Mutex;
 use lapin::{Channel, ExchangeKind};
 use lapin::options::{ExchangeDeclareOptions, ExchangeDeleteOptions};
 use lapin::types::FieldTable;
+use crate::comms::Comms;
 
 pub type DeclareExchange = Pin<Box<dyn Fn(Arc<Mutex<Channel>>) -> Pin<Box<dyn Future<Output = anyhow::Result<()>> + Send>> + Send + Sync>>;
 
-
-
-pub fn create_direct_exchange(exchange: String, options: Option<ExchangeDeclareOptions>, arguments: Option<FieldTable> ) -> DeclareExchange {
-    Box::pin(move | channel|{
+pub fn create_exchange(exchange: String, kind: ExchangeKind, options: Option<ExchangeDeclareOptions>, arguments: Option<FieldTable> ) -> DeclareExchange {
+      Box::pin(move | channel|{
         let exchange = exchange.clone();
         let arguments = arguments.clone();
         let options = options.clone();
+          let kind = kind.clone();
         Box::pin(async move {
             let channel = channel.lock().await;
             let backup_arguments = arguments.clone();
             let backup_options = options.clone();
-            if let Err(err) = channel.exchange_declare(&exchange, ExchangeKind::Direct, options.unwrap_or_else(|| ExchangeDeclareOptions::default()), arguments.unwrap_or_else(|| FieldTable::default())).await {
+            if let Err(err) = channel.exchange_declare(&exchange, kind.clone(), options.unwrap_or_else(|| ExchangeDeclareOptions::default()), arguments.unwrap_or_else(|| FieldTable::default())).await {
+                drop(channel);
                 log::error!("Failed to declare exchange: {}", err);
                 log::warn!("Deleting the old one and creating a new one");
+
+                let channel = Comms::create_channel().await;
+
+                let channel = channel.lock().await;
                 match channel.exchange_delete(&exchange, ExchangeDeleteOptions { if_unused: false, nowait: true}).await {
                     Ok(()) => {
-                        if let Err(err) = channel.exchange_declare(&exchange, ExchangeKind::Direct, backup_options.unwrap_or_else(|| ExchangeDeclareOptions::default()), backup_arguments.unwrap_or_else(|| FieldTable::default())).await {
+                        if let Err(err) = channel.exchange_declare(&exchange, kind.clone(), backup_options.unwrap_or_else(|| ExchangeDeclareOptions::default()), backup_arguments.unwrap_or_else(|| FieldTable::default())).await {
                             log::error!("Failed to declare exchange (Exiting): {}", err);
                             std::process::exit(1);
                         }
@@ -38,4 +43,13 @@ pub fn create_direct_exchange(exchange: String, options: Option<ExchangeDeclareO
             Ok(())
         })
     })
+}
+
+
+pub fn create_direct_exchange(exchange: String, options: Option<ExchangeDeclareOptions>, arguments: Option<FieldTable> ) -> DeclareExchange {
+    create_exchange(exchange, ExchangeKind::Direct, options, arguments)
+}
+
+pub fn create_topic_exchange(exchange: String, options: Option<ExchangeDeclareOptions>, arguments: Option<FieldTable> ) -> DeclareExchange {
+    create_exchange(exchange, ExchangeKind::Topic, options, arguments)
 }
